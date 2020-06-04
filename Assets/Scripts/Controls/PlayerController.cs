65d10156ce4907c22b9139bf409a12fd1c881a5a
denizmavi <denizmavi@sabanciuniv.edu>
2020-06-04 03:30:58 +0300

    Add layering of movement and controls

---
+++ Assets/Scripts/Controls/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using RPG.Movement;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.Control
+{
+    public class PlayerController : MonoBehaviour
+    {
+        void Update()
+        {
+            EvaluateInput();
+        }
+
+        private void EvaluateInput()
+        {
+            if (Input.GetMouseButton(0))
+            {
+                MoveToCursor();
+            }
+        }
+
+        private void MoveToCursor()
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            bool didHit = Physics.Raycast(ray, out RaycastHit raycastHit);
+
+            if (didHit)
+            {
+                GetComponent<Mover>().MoveTo(raycastHit.point);
+            }
+        }
+    }
+}
--- Assets/Scripts/Movement/Mover.cs
+++ Assets/Scripts/Movement/Mover.cs
@@ -4,38 +4,26 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class Mover : MonoBehaviour
+namespace RPG.Movement
 {
-    void Update()
+    public class Mover : MonoBehaviour
     {
-        EvaluateInput();
-        UpdateAnimator();
-    }
-
-    private void UpdateAnimator()
-    {
-        Vector3 velocity = GetComponent<NavMeshAgent>().velocity;
-        Vector3 localVelocity = transform.InverseTransformDirection(velocity);
-        float speed = localVelocity.z;
-        GetComponent<Animator>().SetFloat("forwardSpeed", speed);
-    }
-
-    private void EvaluateInput()
-    {
-        if (Input.GetMouseButton(0))
+        void Update()
         {
-            MoveToCursor();
+            UpdateAnimator();
         }
-    }
 
-    private void MoveToCursor()
-    {
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        bool didHit = Physics.Raycast(ray, out RaycastHit raycastHit);
-
-        if (didHit)
+        private void UpdateAnimator()
         {
-            GetComponent<NavMeshAgent>().destination = raycastHit.point;
+            Vector3 velocity = GetComponent<NavMeshAgent>().velocity;
+            Vector3 localVelocity = transform.InverseTransformDirection(velocity);
+            float speed = localVelocity.z;
+            GetComponent<Animator>().SetFloat("forwardSpeed", speed);
+        }
+
+        public void MoveTo(Vector3 destination)
+        {
+            GetComponent<NavMeshAgent>().destination = destination;
         }
     }
 }
