cca9da24e4fd1e7ce9ea31a513b156b736feefbb
denizmavi <denizmavi@sabanciuniv.edu>
2020-06-05 22:47:16 +0300

    Implement move to target's weapon range function

--- Assets/Scripts/Combat/Fighter.cs
+++ Assets/Scripts/Combat/Fighter.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using RPG.Movement;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,9 +7,30 @@ namespace RPG.Combat
 {
     public class Fighter : MonoBehaviour
     {
-        public void Attack(CombatTarget target)
+        [SerializeField] float weaponRange = 4f;
+        private Transform target;
+        private Mover mover;
+
+        void Start()
         {
-            print("Take that!");
+            mover = GetComponent<Mover>();
+        }
+
+        void Update()
+        {
+            bool isInRange = Vector3.Distance(transform.position, target.position) < weaponRange;
+            if (target != null && !isInRange)
+            {
+                mover.MoveTo(target.position);
+            }
+            else
+            {
+                mover.Stop();
+            }
+        }
+        public void Attack(CombatTarget combatTarget)
+        {
+            this.target = combatTarget.transform;
         }
     }
 }
