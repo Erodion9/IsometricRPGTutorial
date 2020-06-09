a572778d9762bfd207fb3f61ad213ca382889477
denizmavi <denizmavi@sabanciuniv.edu>
2020-06-09 21:23:54 +0300

    Create waypoints and their gizmos for visualization

---
+++ Assets/Scripts/Control/PatrolPath.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.Control
+{
+    public class PatrolPath : MonoBehaviour
+    {
+        const float waypointGizmoRadius = 0.3f;
+        private void OnDrawGizmos()
+        {
+            for (int i = 0; i < transform.childCount; i++)
+            {
+                Transform childTransform = transform.GetChild(i).transform;
+                Gizmos.DrawSphere(childTransform.position, waypointGizmoRadius);
+            }
+        }
+    }
+}
